6f7bb1ddc69f21e680f9a6f9eea825661d101f71
David Ali Caballero Pérez <dcaballe13@cuc.edu.co>
2023-08-28 23:40:26 -0500

    Added style and footer

--- logic.js
+++ logic.js
@@ -8,6 +8,8 @@ function CompareNumbers() {
     document.getElementById("randomNumber").value = randomNumber.toString()
 
     let resultText = userNumber === randomNumber ? "Congratulations!" : "Try again"
+    let resultColor = userNumber === randomNumber ? "green" : "red"
 
     document.getElementById("resultText").value = resultText
+    document.getElementById("result").style.background = resultColor
 }
