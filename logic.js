d44d4bedb3fcf7e7f79368ba74b19571b5f8bd0e
David Ali Caballero Pérez <dcaballe13@cuc.edu.co>
2023-08-28 19:23:53 -0500

    Added logic and some tweaking

---
+++ logic.js
@@ -0,0 +1,13 @@
+function CompareNumbers() {
+    let userNumber = parseInt(document.getElementById("userNumber").value)
+    let max = parseInt(document.getElementById("max").value)
+    let min = parseInt(document.getElementById("min").value)
+
+    let randomNumber = Math.round(min + Math.random() * (max - min))
+
+    document.getElementById("randomNumber").value = randomNumber.toString()
+
+    let resultText = userNumber === randomNumber ? "Congratulations!" : "Try again"
+
+    document.getElementById("resultText").value = resultText
+}
